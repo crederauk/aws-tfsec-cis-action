510be428790c05c6599bc9e64bb3fec80a4ffe27
Gabriel Lee <gabriel.lee@credera.co.uk>
2022-01-17 17:46:15 +0000

    fix gcp examples

--- examples/gcp/bigquery_dataset_encryption_fail_1.tf
+++ examples/gcp/bigquery_dataset_encryption_fail_1.tf
@@ -4,8 +4,4 @@ resource "google_bigquery_dataset" "dataset" {
   description                 = "This is a test description"
   location                    = "US"
   default_table_expiration_ms = 3600000
-
-  default_encryption_configuration {
-    kms_key_name = google_kms_crypto_key.crypto_key.id
-  }
 }
--- examples/gcp/bigquery_table_encryption_fail_1.tf
+++ examples/gcp/bigquery_table_encryption_fail_1.tf
@@ -1,8 +1,4 @@
 resource "google_bigquery_table" "sheet" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "sheet"
-
-  encryption_configuration {
-    kms_key_name = google_kms_crypto_key.crypto_key.id
-  }
 }

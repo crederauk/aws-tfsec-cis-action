e2c5eea8e1a4a82be40b62ac16672e3ffb948416
Eve Welch (Credera) <eve.welch@credera.co.uk>
2022-01-17 14:08:52 +0000

    added azure cis benchmark section 1 checks

---
+++ azure-test.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.92.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_subscription" "primary" {
+}
+
+resource "azurerm_role_definition" "example" {
+  name        = "my-custom-role"
+  scope       = data.azurerm_subscription.primary.id
+
+  permissions {
+    actions     = ["Microsoft.Authorization/locks"]
+  }
+}
